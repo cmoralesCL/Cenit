094211c152f4fd5cd560464a674bd779db7b011f
cmoralesCL <cristian.morales.go@gmail.com>
2025-12-08 23:32:29 -0300

    Subiendo nuevamente todo el proyecto desde cero5

--- src/lib/cookies.ts
+++ src/lib/cookies.ts
@@ -3,14 +3,8 @@ import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adap
 
 export async function getSafeCookieStore(): Promise<ReadonlyRequestCookies> {
     try {
-        const cookieHeader = cookies();
-        const isPromise = cookieHeader instanceof Promise || (cookieHeader && typeof (cookieHeader as any).then === 'function');
-
-        // console.log('[Cookies Debug] cookies() returned:', isPromise ? 'Promise' : 'Value', cookieHeader);
-
-        const cookieStore = isPromise ? await cookieHeader : cookieHeader;
-
-        // console.log('[Cookies Debug] resolved cookieStore:', cookieStore);
+        const cookieStore = await cookies();
+        // console.log('[Cookies Debug] cookies() awaited successfully');
         return cookieStore;
     } catch (error) {
         console.error('Error getting cookie store:', error);
